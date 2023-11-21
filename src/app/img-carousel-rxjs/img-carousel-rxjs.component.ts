878b4311f338ca9dfaca489981175cbd407d75f3
V1KHROV <dmvikhrov@hotmail.com>
2023-11-21 08:29:53 +0300

    First commit, add html & css

--- src/app/img-carousel-rxjs/img-carousel-rxjs.component.ts
+++ src/app/img-carousel-rxjs/img-carousel-rxjs.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+const DEFAULT_OPTIONS: ImgCarouselOptions = {
+  slides: [],
+  activeSlide: 0,
+  hideSlide: null,
+  interval: 5000,
+  indicators: true,
+  controls: true
+}
+
 @Component({
   selector: 'app-img-carousel-rxjs',
   templateUrl: './img-carousel-rxjs.component.html',
@@ -7,9 +16,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImgCarouselRxjsComponent implements OnInit {
 
+  options: ImgCarouselOptions = DEFAULT_OPTIONS;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+/** Slide (img) of image carousel */
+export interface ImgCarouselSlide {
+
+  /** Image source */
+  imageSrc: string;
+  /** Image title */
+  imageTitle?: string;
+  /** Image description */
+  imageDescription?: string;
+  /** Image route */
+  route?: string;
+
+  [key: string]: any;
+}
+
+
+export interface ImgCarouselOptions {
+
+  slides: ImgCarouselSlide[];
+  activeSlide: number;
+  hideSlide: number | null;
+  interval: number;
+  indicators: boolean;
+  controls: boolean;
+
+  [key: string]: any;
 }

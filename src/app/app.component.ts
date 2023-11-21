e5c945a0d67e7f55d2a116ddda840e57aa00d016
V1KHROV <dmvikhrov@hotmail.com>
2023-11-21 11:16:22 +0300

    First work variant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ImgCarouselOptions } from './img-carousel-rxjs/img-carousel-rxjs.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'img-carousel-rxjs';
+
+  options: Partial<ImgCarouselOptions> = {
+    slides: [
+      {
+        imageSrc: 'https://plus.unsplash.com/premium_photo-1669842613532-1f0947aa6fa6?auto=format&fit=crop&q=80&w=1674&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        imageTitle: 'Picture 1'
+      },
+      { 
+        imageSrc: 'https://images.unsplash.com/photo-1508264165352-258db2ebd59b?auto=format&fit=crop&q=80&w=1888&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        imageTitle: 'pic2'
+      },
+      { 
+        imageSrc: 'https://images.unsplash.com/photo-1504788363733-507549153474?auto=format&fit=crop&q=80&w=1734&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        imageTitle: 'pic3'
+      },
+      { 
+        imageSrc: 'https://images.unsplash.com/photo-1506202687253-52e1b29d3527?auto=format&fit=crop&q=80&w=1740&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        imageTitle: 'pic4'
+      },
+      { 
+        imageSrc: 'https://images.unsplash.com/photo-1508144322886-717c284ab392?auto=format&fit=crop&q=80&w=1740&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        imageTitle: 'pic5'
+      },
+      { 
+        imageSrc: 'https://images.unsplash.com/photo-1528840467926-94930468212f?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        imageTitle: 'pic6'
+      },
+    ],
+    interval: 10000,
+    indicators: true,
+    controls: true
+  }
+
 }
--- src/app/img-carousel-rxjs/img-carousel-rxjs.component.ts
+++ src/app/img-carousel-rxjs/img-carousel-rxjs.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable, Subject, interval, startWith, switchMap, tap } from 'rxjs';
 
 const DEFAULT_OPTIONS: ImgCarouselOptions = {
   slides: [],
@@ -12,11 +14,48 @@ const DEFAULT_OPTIONS: ImgCarouselOptions = {
 @Component({
   selector: 'app-img-carousel-rxjs',
   templateUrl: './img-carousel-rxjs.component.html',
-  styleUrls: ['./img-carousel-rxjs.component.css']
+  styleUrls: ['./img-carousel-rxjs.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ImgCarouselRxjsComponent implements OnInit {
 
   options: ImgCarouselOptions = DEFAULT_OPTIONS;
+
+  show$ = new  Observable<number>();
+
+  changeSlide$ = new Subject<number>();
+
+  @Input() set config(value: Partial<ImgCarouselOptions>) {
+    this.options = {...DEFAULT_OPTIONS, ...value};
+    console.log(this.options);
+    this.show$ = this.changeSlide$.pipe(
+      startWith(-1),      
+      switchMap(index => { 
+        console.log(index);       
+        if(index >= 0 && index < this.options.slides.length) {          
+          this.options.hideSlide = this.options.activeSlide;
+          this.options.activeSlide = index;
+        }
+        else this.changeSlide$.next(0);
+        return interval(this.options.interval).pipe(
+          tap(() => {
+            if(!window.document.hidden) {
+              if(this.options.activeSlide + 1 === this.options.slides.length) {
+                this.options.hideSlide = this.options.slides.length - 1;
+                this.options.activeSlide = 0;
+                console.log(this.options.activeSlide);
+              }
+              else {
+                this.options.hideSlide = this.options.activeSlide;
+                this.options.activeSlide++;
+                console.log(this.options.activeSlide);
+              }              
+            }
+          })
+        )
+      })
+    )
+  }
 
   constructor() { }
 
